92cf386fcc6ed0997ab457dab7a643598acb9ffa
Soumik <soumiksaha827@gmail.com>
2026-01-13 14:07:21 +0530

    implement features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,126 @@
-import React from 'react'
+import { Suspense, useEffect, useRef } from "react";
+import StudioLights from "./three/StudioLights";
+import clsx from "clsx";
+import { useMediaQuery } from "react-responsive";
+import MacbookModel from "./models/Macbook";
+import { features, featureSequence } from "../constants/index.jsx";
+import { Html } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import useMacBookStore from "../store/index.js";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const ModelScroll = () => {
+  const groupRef = useRef(null);
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+  const { setTexture } = useMacBookStore();
+
+  useEffect(() => {
+    featureSequence.forEach((feature) => {
+      const v = document.createElement("video");
+      Object.assign(v, {
+        src: feature.videoPath,
+        muted: true,
+        playsInLine: true,
+        preload: "auto",
+        crossOrigin: "a",
+      });
+
+      v.load();
+    });
+  }, []);
+
+  useGSAP(() => {
+    const modelTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#f-canvas",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+        pin: true,
+      },
+    });
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#f-canvas",
+        start: "top center",
+        end: "bottom top",
+        scrub: 1,
+      },
+    });
+
+    if (groupRef.current) {
+      modelTimeline.to(groupRef.current.rotation, {
+        y: Math.PI * 2,
+        ease: "power1.inOut",
+      });
+    }
+
+    timeline
+      .call(() => setTexture("/videos/feature-1.mp4"))
+      .to(".box1", { opacity: 1, y: 0, delay: 1 });
+
+    timeline
+      .call(() => setTexture("/videos/feature-2.mp4"))
+      .to(".box2", { opacity: 1, y: 0 });
+
+    timeline
+      .call(() => setTexture("/videos/feature-3.mp4"))
+      .to(".box3", { opacity: 1, y: 0 });
+
+    timeline
+      .call(() => setTexture("/videos/feature-4.mp4"))
+      .to(".box4", { opacity: 1, y: 0 });
+
+    timeline
+      .call(() => setTexture("/videos/feature-5.mp4"))
+      .to(".box5", { opacity: 1, y: 0 });
+  }, []);
+
+  return (
+    <group ref={groupRef}>
+      <Suspense
+        fallback={
+          <Html>
+            <h1 className="text-white text-3xl uppercase">Loading...</h1>
+          </Html>
+        }>
+        <MacbookModel scale={isMobile ? 0.05 : 0.08} position={[0, -1, 0]} />
+      </Suspense>
+    </group>
+  );
+};
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <section id="features">
+      <h2>See it all in a new light.</h2>
 
-export default Features
+      <Canvas id="f-canvas">
+        <StudioLights />
+        <ambientLight intensity={0.5} />
+        <ModelScroll />
+      </Canvas>
+      <div className="absolute inset-0">
+        {features.map((feature, index) => (
+          <div
+            key={feature.id}
+            className={clsx("box", `box${index + 1}`, feature.styles)}>
+            <img
+              src={feature.icon}
+              alt={feature.highlight}
+              className="w-12 h-12 mb-4"
+            />
+            <p className="text-white font-semibold text-lg mb-2">
+              {feature.highlight}
+            </p>
+            <p>{feature.text}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Features;
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,11 @@ const useMacBookStore = create((set) => ({
     setColor: (color) => set({ color }),
     scale: 0.08,
     setScale: (scale) => set({ scale }),
-    reset: () => set({ color: '#2e2c2e', scale: 0.08 }),
+
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
+
+    reset: () => set({ color: '#2e2c2e', scale: 0.08, texture: '/videos/feature-1.mp4' }),
 }))
 
 export default useMacBookStore;
