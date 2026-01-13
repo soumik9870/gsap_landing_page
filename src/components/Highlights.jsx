5a8e20a69fafd23ffb65797d3df485df60992980
Soumik <soumiksaha827@gmail.com>
2026-01-13 15:24:46 +0530

    implement highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import { useMediaQuery } from "react-responsive";
+import gsap from "gsap";
 
 const Highlights = () => {
-  return (
-    <div>Highlights</div>
-  )
-}
+  const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
+  useGSAP(() => {
+    gsap.to(['.left-column', '.right-column'], {
+      scrollTrigger: {
+        trigger: '#highlights',
+        start: isMobile ? 'bottom bottom' : 'top center'
+      },
+      y: 0,
+      opacity: 1,
+      stagger: 0.5,
+      duration: 1,
+      ease: 'power1.inOut'
+    })
+  })
 
-export default Highlights
+  return (
+    <section id="highlights">
+      <h2>There's never been a better time to upgrade.</h2>
+      <h3>Here's what you get with the MacBook.</h3>
+      <div className="masonry">
+        <div className="left-column">
+          <div>
+            <img src="/laptop.png" alt="laptop" />
+            <p>Fly through demanding tasks up to 9.8x faster.</p>
+          </div>
+          <div>
+            <img src="/sun.png" alt="sun" />
+            <p>
+              A stunning <br /> Retina display.
+            </p>
+          </div>
+        </div>
+        <div className="right-column">
+          <div className="apple-gradient">
+            <img src="/ai.png" alt="AI" />
+            <p>
+              Build for <br />
+              <span>Apple Intelligence.</span>
+            </p>
+          </div>
+          <div>
+            <img src="/battery.png" alt="battery" />
+            <p>
+              up to <span className="green-gradient"> 14 more hours </span>{" "}
+              battery life.{" "}
+              <span span className="text-dark-100">
+                {" "}
+                (Up to 24 hours total.)
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
